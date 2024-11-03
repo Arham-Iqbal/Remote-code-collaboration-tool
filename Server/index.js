7e2db39754d4f6f342d1417f8401de23097d5c72
Arham Iqbal <143219273+Arham-Iqbal@users.noreply.github.com>
2024-11-04 01:45:52 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "remote-code-collaboration-tool-lcvt.vercel.app",
+    origin: "https://remote-code-collaboration-tool-lcvt.vercel.app",
     methods: ["GET", "POST"],
   },
 });
